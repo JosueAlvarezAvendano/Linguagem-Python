089e39e47e5dc4548236ba8344274a00599c2a43
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-17 20:45:45 -0300

    Create Modulos.py

---
+++ CursoEmVideo-Python/Modulos.py
@@ -0,0 +1,17 @@
+# MATH - funções matemática:
+# ceil = Arredondamento para cima
+# floor = Arredondamento para baixo
+# trunc = Corta a parte decimal
+# pow = potencia
+# sqrt = Raiz quadrada
+# factorial = calcular fatorial
+
+# DATETIME - funções de data e tempo
+
+# RANDOM - Gerador de números aleatorios
+# random = gerar numero qualquer mesmo decimal
+#randint = gerar número inteiro
+
+
+import emoji
+print(emoji.emojize('Olá Mundo :earth_americas:', use_aliases=True))
