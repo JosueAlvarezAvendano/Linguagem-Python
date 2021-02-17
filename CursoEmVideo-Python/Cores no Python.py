6406d66162b56e91ced9a6ae947cd301188c5df7
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-17 20:45:55 -0300

    Create Cores no Python.py

---
+++ CursoEmVideo-Python/Cores no Python.py
@@ -0,0 +1,28 @@
+# C O M O   C O L O C A R   C O R E S   N O   P Y T H O N
+# Codigo geral:   \033[Tipo da fonte ; Cor do texto ; Cor do fundo m
+
+# NUMEROS PARA ESTILO:
+# 0 = não coloca estil nenhum
+# 1 = coloca em negrito (mais forte)
+# 4 = Coloca em underline (tudo sublinhado)
+# 7 = Inverte tudo (coloca a cor do fundo na letra e da letra no fundo)
+
+# NUMEROS PARA COR DO TEXTO
+# 30 = Branco
+# 31 = Vermelho
+# 32 = Verde
+# 33 = Amarelo
+# 34 = Azul
+# 35 = Magente (Parece um lisas fraco)
+# 36 = Ciano = (Parece um verde azulado)
+# 37 = Cinza (Cor padrão)
+
+# NUMEROS PARA FUNDO
+# 40 = Branco
+# 41 = Vermelho
+# 42 = Verde
+# 43 = Amarelo
+# 44 = Azul
+# 45 = Magente (Parece um lisas fraco)
+# 46 = Ciano = (Parece um verde azulado)
+# 47 = Cinza (Cor padrão)
