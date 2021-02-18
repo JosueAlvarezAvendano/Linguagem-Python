5efa859e95c9ab9017d44c5780c8cc0324606ff0
Josué Alvarez Avendano <79068117+JosueAlvarezAvendano@users.noreply.github.com>
2021-02-18 20:44:14 -0300

    Update ex003.py
    Exercício 003

--- CursoEmVideo-Python/ex003.py
+++ CursoEmVideo-Python/ex003.py
@@ -1,5 +1,5 @@
 # Crie um programa que leia dois números e mostre a soma entre eles.
-numero_1 = float(input('Digite um número: '))
-numero_2 = float(input('Digite outro número: '))
-soma = numero_1 + numero_2
-print(f'A soma de {numero_1} e {numero_2} é igual a {soma}')
+num1 = float(input('Digite um número: '))
+num2 = float(input('Digite outro número: '))
+soma = num1 + num2
+print(f'A soma de {num1} e {num2} é igual a {soma}')
