1c15ac150a96582570ff41afe5230f70f1e5311a
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-17 22:23:24 -0300

    Anotações do CursoEmVideo - Python

---
+++ CursoEmVideo-Python/Anotações.py
@@ -0,0 +1,7 @@
+''' TIPOS PRIMITIVOS '''
+
+# int -> Números Inteiros (ex.: 7, -4, 0 e 9875)
+# floot -> Números Reais (ex.: 4.5, 0.076, -15.223 e 7.0)
+# bool -> Valores Logicos (ex.: True e False)
+# str -> Valores Caracteres ('Olá', '7.5', ' ')
+
