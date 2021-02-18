5a1f1a0ee13efd246f4143acd66157c856621b18
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-18 20:41:25 -0300

    Exercício 003

---
+++ CursoEmVideo-Python/ex003.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+numero_1 = float(input('Digite um número: '))
+numero_2 = float(input('Digite outro número: '))
+soma = numero_1 + numero_2
+print(f'A soma de {numero_1} e {numero_2} é igual a {soma}')
