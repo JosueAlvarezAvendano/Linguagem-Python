e63a2a2f9f9616746e54ddfd1b62daa1fe5ab1be
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-20 13:54:11 -0300

    Exercício 009

---
+++ CursoEmVideo-Python/ex009.py
@@ -0,0 +1,13 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+numero = int(input('Digite um número: '))
+print(f'A tabuada do número {numero} é:')
+print(f'{numero} X 1 = {numero * 1:>3}')
+print(f'{numero} X 2 = {numero * 2:>3}')
+print(f'{numero} X 3 = {numero * 3:>3}')
+print(f'{numero} X 4 = {numero * 4:>3}')
+print(f'{numero} X 5 = {numero * 5:>3}')
+print(f'{numero} X 6 = {numero * 6:>3}')
+print(f'{numero} X 7 = {numero * 7:>3}')
+print(f'{numero} X 8 = {numero * 8:>3}')
+print(f'{numero} X 9 = {numero * 9:>3}')
+print(f'{numero} X 10 = {numero * 10:>2}')
