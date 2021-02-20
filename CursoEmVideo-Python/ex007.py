b5aa0b285a6a1e14b5886de4288b129019d210f6
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-20 13:53:51 -0300

    Exercício 007

---
+++ CursoEmVideo-Python/ex007.py
@@ -0,0 +1,5 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+np1 = float(input('Digite a 1º nota do aluno: '))
+np2 = float(input('Digite a 2º nota do aluno: '))
+media = (np1 + np2) / 2
+print(f'A com essas notas a média do aluno foi de {media:.2}')
