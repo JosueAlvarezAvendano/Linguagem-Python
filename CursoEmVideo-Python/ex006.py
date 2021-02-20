933ec83a102c60968918c41ec445ce91d63b1da0
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-20 13:53:28 -0300

    Exercício 006

---
+++ CursoEmVideo-Python/ex006.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+numero = int(input('Digite um número: '))
+print(f'O dobro do número {numero} é {numero * 2};')
+print(f'O triplo do número {numero} é {numero * 3};')
+print(f'E a raiz quadra do número {numero} é {numero ** (1/2)}')
