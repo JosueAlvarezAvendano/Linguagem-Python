e4f4ae2ead78364493a2b0774154ba30a899383c
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-22 11:15:15 -0300

    Exercício 005

--- CursoEmVideo-Python/ex005.py
+++ CursoEmVideo-Python/ex005.py
@@ -1,4 +1,3 @@
 # Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
 numero = int(input('Digite um número: '))
-print(f'O Antecessor de {numero} é {numero - 1}')
-print(f'O sucessor de {numero} é {numero + 1}')
+print(f'Analisando o valor {numero}, o seu antecessor é {numero - 1} e o seu sucessor é {numero + 1}')
