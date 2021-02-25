bb4b3d636b92927b710a56b4c03931f28ea25839
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-25 20:34:43 -0300

    Exercício 006

--- CursoEmVideo-Python/ex006.py
+++ CursoEmVideo-Python/ex006.py
@@ -2,4 +2,4 @@
 numero = int(input('Digite um número: '))
 print(f'O dobro do número {numero} é {numero * 2};')
 print(f'O triplo do número {numero} é {numero * 3};')
-print(f'E a raiz quadra do número {numero} é {numero ** (1/2)}')
+print(f'E a raiz quadra do número {numero} é {numero ** (1/2):.2f}')
