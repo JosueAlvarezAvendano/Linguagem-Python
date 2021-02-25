516bbc215fa2823d2ba23f85c1f0f87eeac87aa1
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-25 20:39:22 -0300

    Exercício 012

--- CursoEmVideo-Python/ex012.py
+++ CursoEmVideo-Python/ex012.py
@@ -1,4 +1,4 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 preco_normal = float(input('Qual o preço do produto: R$'))
 preco_desconto = preco_normal *0.95
-print(f'O preço do produto é R${preco_normal:.2f}, na promoção com 5% de desconto o produto saira por R${preco_desconto:.2f}')
+print(f'O produto que custava R${preco_normal:.2f}, na promoção com 5% de desconto o produto saira por R${preco_desconto:.2f}')
