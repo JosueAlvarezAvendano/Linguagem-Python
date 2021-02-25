b42bb8e94e9add750a649144706a31280039e4b1
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-25 20:39:30 -0300

    Exercício 014

---
+++ CursoEmVideo-Python/ex014.py
@@ -0,0 +1,4 @@
+# Escreva um programa que converta uma temperatura digitada em °C e converta para ºF.
+celcio = float(input('Informe a temperatura em ºC: '))
+fahrenheit = (celcio * 9 / 5) + 32
+print(f'A temperatura de {celcio}ºC corresponde a {fahrenheit}ºF')
