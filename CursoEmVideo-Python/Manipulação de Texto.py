abb98eff4eda6995749d3c3e0dfed3961fb7265e
Josué Alvarez Avendano <josue.12sp@gmail.com>
2021-02-28 22:37:12 -0300

    Manipulação de texto

--- CursoEmVideo-Python/Manipulação de Texto.py
+++ CursoEmVideo-Python/Manipulação de Texto.py
@@ -1,3 +1,5 @@
+#FATIAMENTO
+
 frase = 'Curso em Video Python'  #O indice começa no 0 e vai até 0 fim
 print(frase[6])                  #Mostra o que está dentro do indice NomeLista[um numero + 1]
 print(frase[4:10])               #Dentro da lista NomeLista[inicio : mostra ate o numero menos um]
@@ -13,18 +15,18 @@ Um texto é uma unidade linguística de extensão superior à frase.''')
 
 #ANALISE
 
-print(len(frase))                #Mostra quantos espaços(não só os espaços em branco, mas todos) tem a lista[]
+print(len(frase))                #Mostra quantos espaços(mostra a quantidades de str tem mais 1) tem a lista[]
 print(frase.count('o'))          #Mostra quantas vesez aparece o que está dentro dos parenteses
 print(frase.count('o',0,13))     #Mostra quantas vezes aparece o que se pede dentro de um incio e fim (fim-1)
 print(frase.find('deo'))         #Mostra onde(iniciou) foi encontrato o que se pede
 print(frase.find('Josué'))       #Se eu pedir algo que não está na lista, me mostrara -1
-print('Curso' in frase)          #Pergunta se existe algo na lista
+print('Curso' in frase)          #Pergunta se existe algo na frase
 
 #TRANSFORMAÇÂO
 
 print(frase.replace('Python','Android'))  #Troca algo por outra coisa
-print(frase.upper())                      #Coloca tudo em maiusculos
-print(frase.lower())                      #Coloca tudo em minusculo
+print(frase.upper())                      #Coloca tudo em maiúsculos
+print(frase.lower())                      #Coloca tudo em minúsculo
 print(frase.capitalize())                 #Deixa tudo em minusculo e deixa só o primeiro em maiusculos
 print(frase.title())                      #Deixa todos as primeiras letras de palavra em maiuscula
 print(frase.strip())                      #Remove os espaços a mais no começo e no fim (não remove espaços no meio)
@@ -33,7 +35,7 @@ print(frase.lstrip())      #L = Esquerda  #Só vai remover os espaços a mais na
 
 #DIVISÂO
 
-print(frase.split())              #Vai separar cada palavra e transformar em uma noma str e coloca em uma nova lista
+print(frase.split())              #Vai separar cada palavra e transformar em uma str e coloca em uma nova lista
 lista = frase.split()
 
 #JUNÇÂO
